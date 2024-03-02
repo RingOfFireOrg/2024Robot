73069a780fb1ad58eeb9b4dbc5867cdeae58e6be
Anoop Kodadhala <73144205+AnoopKodadhala@users.noreply.github.com>
2024-03-02 01:13:59 -0500

    3rd vision class 😭

---
+++ src/main/java/frc/robot/Vision.java
@@ -0,0 +1,84 @@
+package frc.robot;
+
+import java.util.Optional;
+
+import org.photonvision.EstimatedRobotPose;
+import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
+import org.photonvision.simulation.PhotonCameraSim;
+import org.photonvision.simulation.SimCameraProperties;
+import org.photonvision.simulation.VisionSystemSim;
+import org.photonvision.targeting.PhotonPipelineResult;
+
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.Matrix;
+import edu.wpi.first.math.VecBuilder;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Transform3d;
+import edu.wpi.first.math.numbers.N1;
+import edu.wpi.first.math.numbers.N3;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
+
+/** Add your docs here. */
+public class Vision {
+
+    private PhotonCamera photonCamera;
+    private PhotonPoseEstimator photonEstimator;
+    private double lastEstTimestamp = 0;
+    AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+
+    public Vision() {
+        photonCamera = new PhotonCamera("LL3_PT");
+
+        photonEstimator =
+                new PhotonPoseEstimator(
+                        aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, photonCamera, new Transform3d() //UPDATE TRANSFORM 3D
+                        );
+        photonEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
+
+    }
+
+    public PhotonPipelineResult getLatestResult() {
+        return photonCamera.getLatestResult();
+    }
+
+    public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
+        var visionEst = photonEstimator.update();
+        double latestTimestamp = photonCamera.getLatestResult().getTimestampSeconds();
+        boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp) > 1e-5;
+
+        if (newResult) { 
+            lastEstTimestamp = latestTimestamp;
+        }
+        return visionEst;
+    }
+
+    public Matrix<N3, N1> getEstimationStdDevs(Pose2d estimatedPose) {
+        var estStdDevs = VecBuilder.fill(4, 4, 8); //TODO: Tune
+        var targets = getLatestResult().getTargets();
+        int numTags = 0;
+        double avgDist = 0;
+        for (var tgt : targets) {
+            var tagPose = photonEstimator.getFieldTags().getTagPose(tgt.getFiducialId());
+            if (tagPose.isEmpty()) continue;
+            numTags++;
+            avgDist +=
+                    tagPose.get().toPose2d().getTranslation().getDistance(estimatedPose.getTranslation());
+        }
+        if (numTags == 0) return estStdDevs;
+        avgDist /= numTags;
+        // Decrease std devs if multiple targets are visible
+        if (numTags > 1) estStdDevs = VecBuilder.fill(0.5, 0.5, 1); //TODO: tune
+        // Increase std devs based on (average) distance
+        if (numTags == 1 && avgDist > 4)
+            estStdDevs = VecBuilder.fill(Double.MAX_VALUE, Double.MAX_VALUE, Double.MAX_VALUE);
+        else estStdDevs = estStdDevs.times(1 + (avgDist * avgDist / 30));
+
+        return estStdDevs;
+    }
+
+
+}
--- src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/PhotonVisionSubsystem.java
@@ -1,15 +1,24 @@
 package frc.robot.subsystems;
 
+import java.util.Optional;
 import java.util.function.BooleanSupplier;
 
+import org.photonvision.EstimatedRobotPose;
 import org.photonvision.PhotonCamera;
+import org.photonvision.PhotonPoseEstimator;
+import org.photonvision.PhotonPoseEstimator.PoseStrategy;
 import org.photonvision.common.hardware.VisionLEDMode;
 import org.photonvision.targeting.PhotonPipelineResult;
 
+import edu.wpi.first.apriltag.AprilTagFieldLayout;
+import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Robot;
 
 public class PhotonVisionSubsystem extends SubsystemBase {
 
@@ -19,20 +28,32 @@ public class PhotonVisionSubsystem extends SubsystemBase {
     STANDBY,
     IDLE
   }
+  private double lastEstTimestamp = 0;
 
   private PhotonCamera TomatoSoup;
   private boolean hasTargets;
   private PhotonPipelineResult latestResult;
   private Transform3d bestCameraToTarget;
+  PhotonPoseEstimator photonPoseEstimator;
 
-  CameraStatus limeLightStatus = CameraStatus.IDLE;
+  CameraStatus cameraStatus = CameraStatus.IDLE;
+
+  AprilTagFieldLayout aprilTagFieldLayout = AprilTagFields.k2024Crescendo.loadAprilTagLayoutField();
+
 
   public final double cameraHeight = Units.inchesToMeters(0);   //Meters
   public final double targetHeight = Units.inchesToMeters(0);   //Meters
   public final double cameraPitch = Units.degreesToRadians(0); // Radians
+
+
   
+
   public PhotonVisionSubsystem() {
     TomatoSoup = new PhotonCamera("TomatoSoup");
+    photonPoseEstimator = new PhotonPoseEstimator(
+      aprilTagFieldLayout, PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, TomatoSoup, new Transform3d()
+    );
+    //photonPoseEstimator.setMultiTagFallbackStrategy(PoseStrategy.LOWEST_AMBIGUITY);
   }
 
   @Override
@@ -61,12 +82,38 @@ public class PhotonVisionSubsystem extends SubsystemBase {
   }
 
 
+  public Optional<EstimatedRobotPose> getEstimatedGlobalPose() {
+    var visionEst = photonPoseEstimator.update();
+    double latestTimestamp = TomatoSoup.getLatestResult().getTimestampSeconds();
+    boolean newResult = Math.abs(latestTimestamp - lastEstTimestamp) > 1e-5;
+    if (newResult) {
+      lastEstTimestamp = latestTimestamp;
+    }
+    return visionEst;
+  }
+
+
+
+
+  // public void update(double leftDist, double rightDist, Rotation2d rotation2d) {
+  //     photonPoseEstimator.update(rotation2d, leftDist, rightDist);
+
+  //     var res = TomatoSoup.getLatestResult();
+  //     if (res.hasTargets()) {
+  //         var imageCaptureTime = res.getTimestampSeconds();
+  //         var camToTargetTrans = res.getBestTarget().getBestCameraToTarget();
+  //         var camPose = Constants.kFarTargetPose.transformBy(camToTargetTrans.inverse());
+  //         m_poseEstimator.addVisionMeasurement(
+  //                 camPose.transformBy(Constants.kCameraToRobot).toPose2d(), imageCaptureTime);
+  //     }
+  // }
+
   public CameraStatus getLimeLightStatusStatus() {
-    return limeLightStatus;
+    return cameraStatus;
   }
 
   public void setLimeLightStatus(CameraStatus newStatus) {
-    limeLightStatus = newStatus;
+    cameraStatus = newStatus;
   }
 
   public PhotonCamera getCamera() {
